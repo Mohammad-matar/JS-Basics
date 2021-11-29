c7cf863fc131f5fa04a0bae3c54df0188a1f9689
Mohammad Mater <moe.matar1998@gmail.com>
2021-11-29 18:34:07 +0100

    basic4-step4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,8 @@
+function hello(){
+    var name=document.getElementById("name").value;
+    var surname=document.getElementById("surname").value
+    var city=document.getElementById("city").value
+    alert("name: "+name+"\nsurname: "+surname+"\ncity: "+city);
+    
+}
+
