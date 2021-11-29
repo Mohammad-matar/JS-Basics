b8277db4f8009b693367ff7db359459eb7efb60c
Mohammad Mater <moe.matar1998@gmail.com>
2021-11-29 19:39:35 +0100

    basic6-step6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,5 @@
+function remind(){
+    var firstnumber=document.getElementById("first_number").value;
+    var secondnumber=document.getElementById("second_number").value;
+    alert("the result is "+firstnumber%secondnumber);
+}
