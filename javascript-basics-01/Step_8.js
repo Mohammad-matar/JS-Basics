2cb8e9b542ddf29e64d603b2c6f0528e8ed33bfe
Mohammad Mater <moe.matar1998@gmail.com>
2021-11-29 20:10:53 +0100

    basic9-part9

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,10 @@
+function eighteen(){
+    var yourage=document.getElementById("age").value;
+    if (yourage >= 18) {
+        alert("you're over 18");
+      } 
+      else {
+        alert("you're under 18");
+      }
+     
+}
