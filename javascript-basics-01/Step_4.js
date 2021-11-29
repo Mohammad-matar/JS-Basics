f6fbfa47bcec68334b0eec2d8fe4d8038e6940ec
Mohammad Mater <moe.matar1998@gmail.com>
2021-11-29 19:19:08 +0100

    basic5-step5

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,7 +1,7 @@
 function hello(){
     var name=document.getElementById("name").value;
-    var surname=document.getElementById("surname").value
-    var city=document.getElementById("city").value
+    var surname=document.getElementById("surname").value;
+    var city=document.getElementById("city").value;
     alert("name: "+name+"\nsurname: "+surname+"\ncity: "+city);
     
 }
---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,7 @@
+function addition(){
+    var firstnumber=document.getElementById("first_number").value;
+    var secondnumber=document.getElementById("second_number").value;
+    alert("The result is "+firstnumber*secondnumber);
+    
+}
+
