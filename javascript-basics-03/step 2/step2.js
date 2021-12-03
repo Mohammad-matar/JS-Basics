4bdc69b1453275fa13201ce7233a5c59d5fcdcbe
Mohammad Mater <moe.matar1998@gmail.com>
2021-12-03 20:39:16 +0100

    basic3.step2

---
+++ javascript-basics-03/step 2/step2.js
@@ -0,0 +1,8 @@
+function changeanchor(num){
+    if (num===1) {
+        document.getElementById("texte").style.display="block";
+    }
+    else {
+        document.getElementById("texte").style.display="none";
+    }
+}
